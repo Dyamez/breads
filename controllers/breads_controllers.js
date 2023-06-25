3089ed970adf8bdb17c0dce91bcfcedc65747e50
Dyamez <dyamez@outlook.com>
2023-06-25 11:52:20 -0700

    create and new routes complete

--- controllers/breads_controllers.js
+++ controllers/breads_controllers.js
@@ -2,6 +2,9 @@ const express = require('express');
 const breads = express.Router();
 const Bread = require('../models/bread.js');
 
+
+
+
 // INDEX
 breads.get('/', (req, res) => {
     res.render('Index',
@@ -13,29 +16,12 @@ breads.get('/', (req, res) => {
   // res.send(Bread)
 })
 
-
-/*
-breads.get('/', (req, res) => {
-    res.render('index')
-    //res.send(Bread)
-});
-
-breads.get('/:arrayIndex', (req, res) => {
-    res.send(Bread[req.params.arrayIndex])
-});
-
-
-// SHOW
-breads.get('/:arrayIndex', (req, res) => {
-  if (Bread[req.params.arrayIndex]) {
-    res.render('Show', {
-      bread:Bread[req.params.arrayIndex]
-    })
-  } else {
-    res.render('404')
-  }
+// NEW
+breads.get('/new', (req, res) => {
+  res.render('new')
 })
-*/
+
+//Show
 breads.get('/:arrayIndex', (req, res) => {
   if (Bread[req.params.arrayIndex]) {
     res.render('Show', {
@@ -44,6 +30,45 @@ breads.get('/:arrayIndex', (req, res) => {
   } else {
     res.send('404')
   }
+});
+/*
+// CREATE
+breads.post('/', (req, res) => {
+  if(req.body.hasGluten === 'on') {
+    req.body.hasGluten = 'true'
+  } else {
+    req.body.hasGluten = 'false'
+  }
+  Bread.push(req.body)
+  res.send(Bread)
+})
+
+
+// CREATE
+breads.post('/', (req, res) => {
+  console.log(req.body)
+  if(req.body.hasGluten === 'on') {
+    req.body.hasGluten = 'true'
+  } else {
+    req.body.hasGluten = 'false'
+  }
+  Bread.push(req.body)
+  res.redirect('/breads')
+})
+*/
+
+// CREATE
+breads.post('/', (req, res) => {
+  if (!req.body.image) {
+    req.body.image = 'https://images.unsplash.com/photo-1517686469429-8bdb88b9f907?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
+  }
+  if(req.body.hasGluten === 'on') {
+    req.body.hasGluten = true
+  } else {
+    req.body.hasGluten = false
+  }
+  Bread.push(req.body)
+  res.redirect('/breads')
 })
 
 module.exports = breads;

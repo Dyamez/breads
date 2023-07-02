f72adfaeccae25dc68e2d203e42728f43724bc8a
Dyamez <dyamez@outlook.com>
2023-07-02 13:22:25 -0700

    day 7 breads project done

--- server.js
+++ server.js
@@ -29,10 +29,28 @@ app.get('*', (req, res) => {
     res.send('404')
   })
   
-// MongoDB connection
+// MongoDB connection original code
+/*
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
   () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
 )
+*/
+
+//code working with new version of mongooseDB
+
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connected to MongoDB:', process.env.MONGO_URI)
+    // Continue with your application logic here
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error)
+  })
 
 // listen port  
 app.listen(PORT, () => {

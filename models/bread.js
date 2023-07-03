8e89faf4fd3addd056727d22362b822489f2986f
Dyamez <dyamez@outlook.com>
2023-07-03 16:29:29 -0700

    custom helper methods added

--- models/bread.js
+++ models/bread.js
@@ -7,12 +7,17 @@ const { Schema } = mongoose
 const breadSchema = new Schema({
     name: { type: String, required: true },
     hasGluten: Boolean,
-    image: { type: String, default: 'http://placehold.it/500x500.png' },
+    image: { type: String, default: 'https://ih0.redbubble.net/image.424804829.3332/ap,550x550,12x12,1,transparent,t.u4.png' },
     baker: {
         type: String,
         enum: ['Ronald', 'Grimace', 'Hamburglar', 'Birdie', 'Sundae', 'Fry Kids']
     }
 })
+
+// helper methods 
+breadSchema.methods.getBakedBy = function(){
+    return `${this.name} was baked with love by ${this.baker}`
+  }  
 
 // model and export 
 const Bread = mongoose.model('Bread', breadSchema)
